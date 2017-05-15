12aeed874471e1b512911f4c9d67fe90742a5c77
Angie Barrantes <angiefiorellacat@gmail.com>
2017-05-15 16:00:17 -0500

    I've updated my application, now it is consuming an API

--- README.md
+++
@@ -1,2 +0,0 @@
-# login-user
-I've created a basic and nice login.
--- js/app.js
+++ js/app.js
@@ -1,34 +1,31 @@
 var app = angular.module('myApp', []);
-app.controller('myController', myFunction);
-function myFunction($scope, $window){
-	$scope.user = "admin@rcp.pe";
-	$scope.password = 12345;
-	$scope.messageId = 0;
-	$scope.message = "";
-	$scope.class="invisible";
-	/*if (myForm.myUser.$valid === true){
-		$scope.messageId = 1;
-	} else {
-		$scope.messageId = 2;
-	}
-	if ($scope.messageId === 1){
-		$scope.message = "Valido";
-	} else {
-		$scope.message = "Inválido";
-	}*/
-	$scope.setData = function(){
-		/*if($scope.myUser === "admin@rcp.pe" && $scope.myPassword === 12345){
-			if ($scope.class==="invisible"){
-					$scope.class="visible";
-			} else {
-					$scope.class="invisible";
-			}
-		}*/
-		if($scope.myUser === "admin@rcp.pe" && $scope.myPassword === 12345){
-			window.alert("Usuario autenticado correctamente");
-		} else {
-			window.alert("Usuario y/o contraseña incorrecta");
-		}
+app.controller('myController', ['$http', '$scope', function($http, $scope){
+	$scope.user = {
+		email: null,
+		password: null
 	};
-
-};
+	$scope.error = '';
+	$scope.login = function(){
+		if( $scope.user.email === null || $scope.user.password === null){
+			console.log("no params");
+			return;
+		}
+		$http({
+			method: 'POST',
+			url: 'http://sandbox.urbaner.com/api/client/authenticate',
+			data: JSON.stringify($scope.user),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}).then(function(response){
+			if(response.status == 200 && response.data.status=='success'){
+				window.location.href='../shopping-list/shopping.html';
+			} else {
+				$scope.error= 'Usuario y/o contraseña inválidos';
+			}
+		}, function(error){
+				console.log(error);
+				$scope.error= 'Usuario y/o contraseña inválidos';
+		});
+	};
+}]);
